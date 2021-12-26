31100e0fdefb6e749d68fd7b98f6919a7e6e8389
Bruno Di Lella <brgoncalves@hotmail.com.br>
2021-12-26 13:05:18 -0300

    Swagger

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import express, { NextFunction, Request, Response } from 'express';
 import "express-async-errors";
+import swaggerUi from 'swagger-ui-express';
+
 import { routes } from './routes';
+import swaggerFile from './swagger.json';
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(routes);
 
